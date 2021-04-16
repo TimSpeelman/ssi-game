a579cca897be39bb71131657be01ec6a1ae095e3
Tim Speelman <timspeelman@live.nl>
2021-04-16 10:53:44 +0200

    Improve inspector styling

--- src/ui/components/ActorInspector.tsx
+++ src/ui/components/ActorInspector.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@material-ui/core';
+import { List, ListItem, ListItemText, Typography } from '@material-ui/core';
 import React from 'react';
 import { actorImage } from '../../config/actorImage';
 import { Actor } from '../../data/actor/Actor';
@@ -18,17 +18,24 @@ export function ActorInspector({ actor, assets }: Props) {
             <img src={actorImage(actor.image)} style={{ height: '6rem' }} />
 
             <Typography variant="h6">Assets</Typography>
-            <ul>
+            <List dense>
                 {assets.length > 0 ? (
-                    assets.map((a, i) => <li key={i}>{assetToString(a)}</li>)
+                    assets.map((a, i) => (
+                        <ListItem key={i}>
+                            {/* <Tooltip title={assetToString(a)}> */}
+                            <ListItemText primary={ucFirst(a.type)} secondary={assetFieldsToString(a)} />
+                            {/* <span>
+                                    &lt;<strong>{ucFirst(a.type)}</strong> {getFirstKVPair(a)}&gt;
+                                </span> */}
+                            {/* </Tooltip> */}
+                        </ListItem>
+                    ))
                 ) : (
-                    <ul>
-                        <li>
-                            <small>- Geen -</small>
-                        </li>
-                    </ul>
+                    <ListItem>
+                        <ListItemText primary={'- geen -'} />
+                    </ListItem>
                 )}
-            </ul>
+            </List>
         </div>
     );
 }
@@ -38,6 +45,29 @@ function assetToString(a: Asset) {
     return `<${ucFirst(a.type)} ${keys
         .filter((k) => k !== 'kind' && k !== 'type')
         // @ts-ignore
-        .map((k) => `${k}: ${a[k]}`)
+        .map((k) => `${k}:${a[k]}`)
         .join(' ')}>`;
+}
+
+function assetFieldsToString(a: Asset) {
+    const keys = Object.keys(a);
+    return (
+        keys
+            .filter((k) => k !== 'kind' && k !== 'type')
+            // @ts-ignore
+            .map((k) => `${k}:${a[k]}`)
+            .join(' | ')
+    );
+}
+
+function getFirstKVPair(a: Asset) {
+    const keys = Object.keys(a);
+    return (
+        keys
+            .filter((k) => k !== 'kind' && k !== 'type' && k !== 'id')
+            .slice(0, 1)
+            // @ts-ignore
+            .map((k) => `${k}:${a[k]}`)
+            .join('')
+    );
 }

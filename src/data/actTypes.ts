b8f592467f77235e35225ec022435c50c29a33c5
Tim Speelman <timspeelman@live.nl>
2021-04-21 19:39:29 +0200

    Cleanup

--- src/data/actTypes.ts
+++
@@ -1,6 +0,0 @@
-export const actTypes = [
-    { type: 'issuance', desc: 'Uitgifte van gegevens' },
-    { type: 'authn', desc: 'Authenticatie' },
-    { type: 'share-req', desc: 'Gegevensverzoek' },
-    { type: 'share', desc: 'Gegevenspresentatie' },
-];

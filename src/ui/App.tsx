2899700d089865fed8365231dc8c1d9ba48f481e
Tim Speelman <timspeelman@live.nl>
2021-05-03 20:48:48 +0200

    Move file controls to topbar

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,17 +1,60 @@
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
+import { Clear, Restore, RestorePage, Save } from '@material-ui/icons';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadScenarioFromFile } from '../persistence/loadScenarioFromFile';
+import { saveScenarioToFile } from '../persistence/saveScenarioToFile';
+import { ScenarioActions } from '../state/scenario/actions';
+import { selectScenario } from '../state/scenario/selectors';
 import { NetworkCanvas } from './components/NetworkCanvas';
 
 export function App() {
+    const scenario = useSelector(selectScenario);
+
+    const dispatch = useDispatch();
+
+    const clear = () => confirm('Weet je zeker dat je alles wilt wissen?') && dispatch(ScenarioActions.CLEAR());
+
+    const reset = () => confirm('Weet je zeker dat je alles terug wilt zetten?') && dispatch(ScenarioActions.RESET());
+
+    const saveToFile = () => saveScenarioToFile(scenario);
+
+    function loadFromFile(e: any) {
+        const files = e.target.files;
+        console.log('FILES', files);
+        if (!files || files.length !== 1) return;
+
+        loadScenarioFromFile(files[0])
+            .then((scenario) => {
+                dispatch(ScenarioActions.SET_SCENARIO({ scenario: scenario.props }));
+                alert('Bestand geladen!');
+            })
+            .catch((e) => alert(e));
+    }
+
     return (
         <div className="fill">
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
+                    {/* <IconButton edge="start" color="inherit" aria-label="menu">
                         <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6">Identity Game</Typography>
+                    </IconButton> */}
+                    <Typography variant="h6" style={{ flexGrow: 1 }}>
+                        Identity Game
+                    </Typography>
+                    <Button color={'inherit'} onClick={clear} style={{ marginRight: '.5rem' }}>
+                        <Clear /> Legen
+                    </Button>
+                    <Button color={'inherit'} onClick={reset} style={{ marginRight: '.5rem' }}>
+                        <Restore /> Reset
+                    </Button>
+                    <Button color={'inherit'} onClick={saveToFile} style={{ marginRight: '.5rem' }}>
+                        <Save /> Opslaan
+                    </Button>
+                    <Button color={'inherit'} component={'label'}>
+                        <RestorePage /> Laden
+                        <input type="file" hidden value={undefined} onChange={(e) => loadFromFile(e)} />
+                    </Button>
                 </Toolbar>
             </AppBar>
             <NetworkCanvas />
--- src/ui/components/NetworkControls.tsx
+++ src/ui/components/NetworkControls.tsx
@@ -1,12 +1,9 @@
 import { Button, Divider } from '@material-ui/core';
-import { Add, Clear, Restore, RestorePage, Save } from '@material-ui/icons';
+import { Add } from '@material-ui/icons';
 import React, { Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadScenarioFromFile } from '../../persistence/loadScenarioFromFile';
-import { saveScenarioToFile } from '../../persistence/saveScenarioToFile';
 import { ScenarioActions } from '../../state/scenario/actions';
 import {
-    selectScenario,
     selectSelectedActor,
     selectSelectedStep,
     selectSnackbarIsOn,
@@ -25,27 +22,7 @@ export function NetworkControls() {
     const usedActors = useSelector(selectUsedActors);
     const unusedActors = useSelector(selectUnusedActors);
     const snackbarIsOn = useSelector(selectSnackbarIsOn);
-    const scenario = useSelector(selectScenario);
     const dispatch = useDispatch();
-
-    const clear = () => confirm('Weet je zeker dat je alles wilt wissen?') && dispatch(ScenarioActions.CLEAR());
-
-    const reset = () => confirm('Weet je zeker dat je alles terug wilt zetten?') && dispatch(ScenarioActions.RESET());
-
-    const saveToFile = () => saveScenarioToFile(scenario);
-
-    function loadFromFile(e: any) {
-        const files = e.target.files;
-        console.log('FILES', files);
-        if (!files || files.length !== 1) return;
-
-        loadScenarioFromFile(files[0])
-            .then((scenario) => {
-                dispatch(ScenarioActions.SET_SCENARIO({ scenario: scenario.props }));
-                alert('Bestand geladen!');
-            })
-            .catch((e) => alert(e));
-    }
 
     return (
         <div>
@@ -54,22 +31,6 @@ export function NetworkControls() {
 
             {selectedActor && <ActorInspector actor={selectedActor.actor} assets={selectedActor.assets} />}
             {selectedActor && <Divider />}
-
-            <div style={{ padding: '1rem' }}>
-                <Button variant={'outlined'} onClick={clear}>
-                    <Clear /> Legen
-                </Button>{' '}
-                <Button variant={'outlined'} onClick={reset}>
-                    <Restore /> Reset
-                </Button>{' '}
-                <Button variant={'outlined'} onClick={saveToFile}>
-                    <Save /> Opslaan
-                </Button>{' '}
-                <Button variant={'outlined'} component={'label'}>
-                    <RestorePage /> Laden
-                    <input type="file" hidden value={undefined} onChange={(e) => loadFromFile(e)} />
-                </Button>
-            </div>
 
             <Divider />
             <div style={{ padding: '1rem' }}>

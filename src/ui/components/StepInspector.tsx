110dfb6698a9ab49227088e01dea780b5115e5ed
Tim Speelman <timspeelman@live.nl>
2021-05-10 13:12:35 +0200

    Display erros in StepInspector

--- src/ui/components/StepInspector.tsx
+++ src/ui/components/StepInspector.tsx
@@ -1,5 +1,5 @@
 import { Typography } from '@material-ui/core';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { ScenarioStepDescription } from '../../model/view/ScenarioStepDescription';
 import { StepLabel } from './StepLabel';
 
@@ -12,8 +12,30 @@ export function StepInspector({ step }: Props) {
     return (
         <div style={{ padding: '1rem' }}>
             <Typography variant="h5">Geselecteerde actie</Typography>
+
+            {/* Same label as shown in the StepSequence */}
             <StepLabel step={step} />
+
+            {/* Additional explanation */}
             <p style={{ marginBottom: '1em' }}>{step.action.long}</p>
+
+            {/* Optional validation errors */}
+            {!step.success && (
+                <Fragment>
+                    <Typography variant="h6" style={{ color: 'red' }}>
+                        Deze stap mislukt..
+                    </Typography>
+                    <ul>
+                        {step.validation
+                            .filter((v) => !v.success)
+                            .map((v, i) => (
+                                <li key={i}>{v.message}</li>
+                            ))}
+                    </ul>
+                </Fragment>
+            )}
+
+            {/* Outcome list */}
             <Typography variant="h6">Uitkomsten</Typography>
             {step.outcomes.length > 0 ? (
                 <ul>

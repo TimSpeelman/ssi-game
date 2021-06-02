a6cdffb4f88e9e11cbc92df76f34cab97a8ff62d
Tim Speelman <timspeelman@live.nl>
2021-06-02 15:23:19 +0200

    Reorganise

--- src/state/scenario/selectors.ts
+++ src/state/scenario/selectors.ts
@@ -15,21 +15,31 @@ import { w1th } from '../../util/w1th';
 import { RootState } from './state';
 
 export const root = (r: any): RootState => r.scenario;
-export const selectActiveSidebarTab = (r: any): SidebarTab => root(r).activeSidebarTab;
-export const selectScenarioDesc = (r: any): ScenarioDesc => computeScenarioFromDefinition(root(r).scenario);
-export const selectScenarioDef = (r: any): ScenarioDef => root(r).scenario;
 
+// Definition
+export const selectScenarioDef = (r: any): ScenarioDef => root(r).scenario;
 export const selectScenarioConfiguration = (r: any): ScenarioDef => root(r).scenario;
 export const selectScenarioMeta = (r: any): ScenarioMeta => selectScenarioConfiguration(r).meta;
+export const selectActorTypes = (r: any): ActorType[] => Object.values(actorTypes);
+export const selectUsedActors = (r: any): Actor[] =>
+    root(r).scenario.actors.map((a) => definitionToActor(a.definition));
+/** Involved actors are actors that are involved in at least one step */
+export const selectInvolvedActors = (r: any): Record<string, true> =>
+    selectSteps(r).reduce((ids, step) => ({ ...ids, [step.action.from.id]: true, [step.action.to.id]: true }), {});
+
+// Description : Active
+export const selectScenarioDesc = (r: any): ScenarioDesc => computeScenarioFromDefinition(root(r).scenario);
 export const selectSteps = (r: any): StepDesc[] => selectScenarioDesc(r).steps;
 export const selectInitialState = (r: any): StateDesc => selectScenarioDesc(r).initial;
 
-export const selectActiveState = (r: any): StateDesc =>
-    w1th(selectActiveStep(r), (currentStep) => (currentStep ? currentStep.result : selectInitialState(r)));
 export const selectFailedStep = (r: any): StepDesc | undefined =>
     w1th(selectScenarioDesc(r).failingAtIndex, (index) =>
         index !== undefined && index >= 0 ? selectSteps(r)[index] : undefined,
     );
+
+// Description : Active
+export const selectActiveState = (r: any): StateDesc =>
+    w1th(selectActiveStep(r), (currentStep) => (currentStep ? currentStep.result : selectInitialState(r)));
 export const selectActiveStepSerialized = (r: any): ActionDef<any> | undefined =>
     root(r).scenario.steps.find((s) => s.id === root(r).activeStepId);
 export const selectActiveStep = (r: any): StepDesc | undefined =>
@@ -37,20 +47,21 @@ export const selectActiveStep = (r: any): StepDesc | undefined =>
 export const selectActiveStepId = (r: any): string | undefined => root(r).activeStepId;
 export const selectActiveStepIndex = (r: any): number =>
     selectSteps(r).findIndex((step) => step.action.id === selectActiveStepId(r));
+
+// Selection
 export const selectSelectedStep = (r: any): StepDesc | undefined =>
     w1th(selectSelectedStepId(r), (id) => (!id ? undefined : selectSteps(r).find((step) => step.action.id === id)));
 export const selectSelectedStepId = (r: any): string | undefined => root(r).selectedStepId;
 export const selectSelectedActorId = (r: any): string | undefined => root(r).selectedActorId;
-export const selectActorTypes = (r: any): ActorType[] => Object.values(actorTypes);
-export const selectUsedActors = (r: any): Actor[] =>
-    root(r).scenario.actors.map((a) => definitionToActor(a.definition));
 
 export const selectSelectedActor = (r: any): ActorStateDesc | undefined =>
     w1th(root(r).selectedActorId, (id) => (id ? selectActiveState(r).actors[id] : undefined));
 
-/** Involved actors are actors that are involved in at least one step */
-export const selectInvolvedActors = (r: any): Record<string, true> =>
-    selectSteps(r).reduce((ids, step) => ({ ...ids, [step.action.from.id]: true, [step.action.to.id]: true }), {});
+// Sidebar Navigation
+export const selectActiveSidebarTab = (r: any): SidebarTab => root(r).activeSidebarTab;
 
-export const selectSnackbarIsOn = (r: any) => root(r).snackbarOn;
+// Display Meta Dialog
 export const selectShowMeta = (r: any) => root(r).showMeta;
+
+// Options
+export const selectSnackbarIsOn = (r: any) => root(r).snackbarOn;

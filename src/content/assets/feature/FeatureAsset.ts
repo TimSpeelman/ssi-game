fcaf190363b7abc32cec798e496bbdccce140279
Tim Speelman <timspeelman@live.nl>
2021-07-10 16:05:01 +0200

    Cleanup

--- src/content/assets/feature/FeatureAsset.ts
+++
@@ -1,5 +0,0 @@
-import { FaceFeature } from './FaceFeature';
-import { FingerprintFeature } from './FingerprintFeature';
-
-/** The FeatureAsset union type describes any kind of inherent (non-transferrable) feature or trait that an Actor has. */
-export type FeatureAsset = FaceFeature | FingerprintFeature;
--- src/content/assets/physical/PhysicalAsset.ts
+++
@@ -1,5 +0,0 @@
-import { GovPassport } from './GovPassport';
-import { PUF } from './PUF';
-
-/** The PhysicalAsset union type describes any kind of physical object that an Actor possesses. */
-export type PhysicalAsset = PUF | GovPassport;

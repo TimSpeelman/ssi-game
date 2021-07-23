05390bba5e99e23cadeec042f7e1be7a895aa3d9
Tim Speelman <timspeelman@live.nl>
2021-07-23 15:05:40 +0200

    Include project name in saved file name

--- src/persistence/saveProjectToFile.ts
+++ src/persistence/saveProjectToFile.ts
@@ -4,6 +4,6 @@ import { PersistedProject } from '../state/scenario/persistence';
 export function saveProjectToFile(scenario: PersistedProject) {
     const blob = new Blob([JSON.stringify(scenario)], { type: 'application/json;charset=utf-8' });
     const date = new Date().toLocaleString().replace(/:/g, '.');
-    const fileName = `SSI-Game Scenario ${date}.json`;
+    const fileName = `SSI-Game Scenario ${date} ${scenario.name}.json`;
     FileSaver.saveAs(blob, fileName);
 }

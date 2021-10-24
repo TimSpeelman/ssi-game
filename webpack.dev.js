4e01b20005ec42164f3f44b75f2a68ff3258e2e2
Tim Speelman <timspeelman@live.nl>
2021-10-24 13:15:16 +0200

    Update webpack for security reasons

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,12 +2,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const path = require('path');
-const os = require('os');
-const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const DEV_SERVER_PORT = 8000;
 const PROTO = 'http';
@@ -19,19 +14,7 @@ module.exports = merge(common, {
         historyApiFallback: true,
         port: DEV_SERVER_PORT,
         host: '0.0.0.0', // To enable access over LAN
-        openPage: [`${PROTO}://localhost:${DEV_SERVER_PORT}/`],
-
-        // For LAN access, print our local IP addresses.
-        before: function (app, server, compiler) {
-            const nets = os.networkInterfaces();
-            const netNames = Object.keys(nets);
-            console.log(`You are being served at ${PROTO}://localhost:${DEV_SERVER_PORT}/`);
-            console.log('Or access externally at one of:');
-            netNames.forEach((netName) =>
-                console.log('-', `${PROTO}://` + nets[netName][0].address + ':' + DEV_SERVER_PORT),
-            );
-            console.log();
-        },
+        open: [`${PROTO}://localhost:${DEV_SERVER_PORT}/`],
     },
     plugins: [
         ...common.plugins,
